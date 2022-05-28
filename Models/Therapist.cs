a2743daa720438f619adeae6ecdd9d5cac231393
Emily Weber <ekweber35@gmail.com>
2022-05-28 11:03:42 -0400

    Added Required Attributes

--- Models/Therapist.cs
+++ Models/Therapist.cs
@@ -5,12 +5,25 @@ using System.ComponentModel.DataAnnotations;
 namespace BreathWebAPI.Models
 {
     public class Therapist {
+        
         public int TherapistId { get; set; }
+
+        [Required]
         public string FullName { get; set; }
+
+        [Required]
         public string BusinessName { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
+
+        [Required]
         public string Specialty { get; set; }
     }
 }
