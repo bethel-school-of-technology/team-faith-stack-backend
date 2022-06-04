2223808bd409e38b63f9c4d4ee47ad3a8513710e
Emily Weber <ekweber35@gmail.com>
2022-06-04 12:51:48 -0400

    added jwt authorization

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BreathWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BreathWebAPI.Controllers
 {
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BreathWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BreathWebAPI.Controllers
 {
@@ -21,7 +22,7 @@ namespace BreathWebAPI.Controllers
         }
 
         // GET: api/Users
-        [HttpGet]
+        [HttpGet, Authorize]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
           if (_context.Users == null)
@@ -32,7 +33,7 @@ namespace BreathWebAPI.Controllers
         }
 
         // GET: api/Users/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), Authorize]
         public async Task<ActionResult<User>> GetUser(int id)
         {
           if (_context.Users == null)
@@ -51,7 +52,7 @@ namespace BreathWebAPI.Controllers
 
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        [HttpPut("{id}"), Authorize]
         public async Task<IActionResult> PutUser(int id, User user)
         {
             if (id != user.Id)
@@ -82,21 +83,57 @@ namespace BreathWebAPI.Controllers
 
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost, Authorize]
+        //public async Task<ActionResult<User>> PostUser(User user)
+        //{
+          //if (_context.Users == null)
+          //{
+              //return Problem("Entity set 'BreathDbContext.Users'  is null.");
+          //}
+            //_context.Users.Add(user);
+            //await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        //}
+
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
           if (_context.Users == null)
           {
-              return Problem("Entity set 'BreathDbContext.Users'  is null.");
+              return BadRequest("Invalid client request");
           }
+
+          if (user.UserName == user.UserName && user.Password == user.Password && user.FirstName == user.FirstName && user.LastName == user.LastName && user.Email == user.Email)
+          {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:7102",
+                    audience: "https://localhost:7102",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+          
+                );
+          
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+
+            return Ok(new AuthResponse { Token = tokenString });
+          }
+
+          return Unauthorized();
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUser", new { id = user.Id }, user);
+
         }
 
+
         // DELETE: api/Users/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}"), Authorize]
         public async Task<IActionResult> DeleteUser(int id)
         {
             if (_context.Users == null)
